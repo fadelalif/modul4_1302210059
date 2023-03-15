657e0c1369397a3e87cb655cec7238e6233660de
fadelalif <126957890+fadelalif@users.noreply.github.com>
2023-03-15 15:39:59 +0700

    Pertama

---
+++ modul4_1302210059/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul4_1302210059
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
